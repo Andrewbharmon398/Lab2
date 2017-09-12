d4c7558ec5a65d8ac48866ccb10cfb78247ab63c
Andrewbharmon398 <andydrew297@gmail.com>
2017-09-12 13:10:49 -0400

    Added tests

--- test_DList.py
+++ test_DList.py
@@ -2,8 +2,8 @@
 
 #----------------------------------------------------------------------
 # test_DList.py
-# Gavyn Partlow
-# 09/06/2017
+# Dave Reed
+# 08/20/2013
 #----------------------------------------------------------------------
 
 import sys
@@ -102,6 +102,159 @@ class DListTest(unittest.TestCase):
         self.check_list(a, b)
 
         self.assertRaises(ValueError, a.remove, 10)
+
+    def test_len(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        self.assertEqual(a.__len__(), 5)
+        b = DList()
+        self.assertEqual(b.__len__(), 0)
+
+    def test_iter(self):
+        pass
+
+    def test_find(self):
+        a = DList()
+        self.assertEqual(a._find(3), "Position does not exist")
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        self.assertEqual(a._find(2), DListNode(3))
+        self.assertEqual(a._find(-2), DListNode(6))
+
+    def test_getItem(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        self.assertEqual(a.__getitem__(2), 3)
+        self.assertEqual(a.__getitem__(3), 6)
+
+    def test_setItem(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        a.__setitem__(1, 4)
+        self.assertEqual(a.__getitem__(1), 4)
+        a.__setitem__(0, 6)
+        self.assertEqual(a.__getitem__(0), 6)
+
+    def test_delItem(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        a.__delitem__(0)
+        a.__delitem__(0)
+        a.__delitem__(0)
+        a.__delitem__(0)
+        a.__delitem__(0)
+        self.assertEqual(a.__len__(), 0)
+
+    def test_delete(self):
+        pass
+
+    def test_append(self):
+        a = DList()
+        a.append(1)
+        self.assertEqual(a.__len__(), 1)
+        self.assertEqual(a.__getitem__(0), 1)
+        a.append(3)
+        self.assertEqual(a.__len__(), 2)
+        self.assertEqual(a.__getitem__(1), 3)
+
+    def test_insert(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        a.insert(2, 4)
+        a.insert(7, 2)
+        self.assertEqual(a.__getitem__(2), 7)
+        self.assertEqual(a.__getitem__(5), 2)
+        b = DList()
+
+    def test_min(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        self.assertEqual(a.__min__(), 3)
+
+    def test_max(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        self.assertEqual(a.__max__(), 9)
+
+    def test_extend(self):
+        checkList = [1, 3, 5, 7, 9, 8, 6, 4, 2]
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        a.extend(checkList)
+        self.assertEqual(a.count(1), 1)
+        self.assertEqual(a.count(9), 2)
+        self.assertEqual(a.count(6), 2)
+        self.assertEqual(a.__len__(), 14)
+
+    def test_count(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        a.append(7)
+        a.append(3)
+        a.append(9)
+        a.append(9)
+        a.append(7)
+        self.assertEqual(a.count(3), 2)
+        self.assertEqual(a.count(5), 1)
+        self.assertEqual(a.count(6), 1)
+        self.assertEqual(a.count(7), 3)
+        self.assertEqual(a.count(9), 3)
+
+    def test_index(self):
+        a = DList()
+        a.append(7)
+        a.append(9)
+        a.append(3)
+        a.append(6)
+        a.append(5)
+        self.assertEqual(a.index(7), 0)
+        self.assertEqual(a.index(9), 1)
+        self.assertEqual(a.index(3), 2)
+        self.assertEqual(a.index(6), 3)
+        self.assertEqual(a.index(5), 4)
+
+
+    def test_reverse_iter(self):
+        pass
 
 #----------------------------------------------------------------------
 
